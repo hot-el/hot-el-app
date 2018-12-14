fffbc56c0a6db1c0f8ea5828743e7d08691e4dfd
PM <machpawel95@gmail.com>
2018-12-14 17:52:52 +0100

    Added component register

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,19 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { CrudModule } from './crud/crud.module';
+
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    CrudModule,
     AppRoutingModule
   ],
   providers: [],
