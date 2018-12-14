68601cf51883767211db1a49553071e746de88d6
PM <machpawel95@gmail.com>
2018-12-14 16:21:55 +0100

    Added home and login component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CrudModule } from './crud/crud.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    CrudModule,
     AppRoutingModule
   ],
   providers: [],
