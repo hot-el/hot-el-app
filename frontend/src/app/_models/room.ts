8535139f759226241c58f58ebaaa672272e09eff
PM <machpawel95@gmail.com>
2018-12-15 18:02:26 +0100

    Fixed room model

--- frontend/src/app/_models/room.ts
+++ frontend/src/app/_models/room.ts
@@ -6,12 +6,4 @@ export class Room {
     floor: number;
     state: string;
     equipment: RoomEquipment[];
-
-    public addEquipment(equipment: RoomEquipment) {
-        this.equipment.push(equipment);
-    }
-
-    public deleteEquipment(id: number) {
-        this.equipment.splice(this.equipment.findIndex(x => x.id == id), 1)
-    }
 }
