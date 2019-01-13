8fe1d326c1b33599a90fbaacdfd4563c158110a1
FransuaSzatowalo <machpawel95@gmail.com>
2019-01-13 13:13:49 +0100

    Fixed relations in user repository

--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -1,15 +1,29 @@
-import {DefaultCrudRepository, juggler} from '@loopback/repository';
-import {User} from '../models';
+import {DefaultCrudRepository, juggler, HasOneRepositoryFactory, repository} from '@loopback/repository';
+import {User, Person} from '../models';
 import {DbDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import {inject, Getter} from '@loopback/core';
+import { PersonRepository } from './person.repository';
 
 export class UserRepository extends DefaultCrudRepository<
   User,
   typeof User.prototype.id
 > {
+  public readonly person: HasOneRepositoryFactory<
+    Person,
+    typeof Person.prototype.id
+  >;
+
   constructor(
     @inject('datasources.db') dataSource: DbDataSource,
+
+    @repository.getter('PersonRepository')
+    getPersonRepository: Getter<PersonRepository>,
   ) {
     super(User, dataSource);
+
+    this.person = this._createHasOneRepositoryFactoryFor(
+      'person',
+      getPersonRepository,
+    )
   }
 }
